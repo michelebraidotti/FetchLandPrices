2f6871b79e2cc620066e02218f638bf3a0ba2d28
Michele Braidotti <michele.braidotti@gmail.com>
2016-05-18 08:30:06 +0200

    Some small fixes

--- show_offers.py
+++ show_offers.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from landprices.persistence.file_persistence import FilePersistence
 
@@ -6,9 +7,12 @@ p = FilePersistence()
 offers = p.load_from_storage()
 print("source\tid_as_from_source\tlink\ttitle\tlocation\tprice\tarea\tdate_retrieved\twas_updated")
 for o in offers:
+    price = o.price
+    price = re.sub(",\d+", "", price)
+    price = re.sub(r"\.", "", price)
     print(
         o.source + "\t" + o.id_as_from_source + "\t" + o.link + "\t" + o.title + "\t" + o.location
-        + "\t" + o.price + "\t" + o.area + "\t" + o.date_retrieved + "\t" + str(o.was_updated))
+        + "\t" + price + "\t" + o.area + "\t" + o.date_retrieved + "\t" + str(o.was_updated))
 
 # offers_dict = []
 # for o in offers:
